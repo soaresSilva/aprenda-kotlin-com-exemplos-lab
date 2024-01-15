a6ab00fb3d2e3e840db8cda9da276bf7c003e483
Alisson Soares <87246547+soaresSilva@users.noreply.github.com>
2024-01-15 12:31:47 -0300

    Subindo o desafio Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+class Usuario(val name: String, val email: String) {
+     // Additional properties and methods can be added here
+}
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60) {
+    
+}
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+	
     val inscritos = mutableListOf<Usuario>()
     
+    /**
+     * Enrolls a user in the formation.
+     * @param usuario The user to be enrolled.
+     */
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) { // if usuario no enrolls, usuario accept
+            inscritos.add(usuario)
+            println("${usuario.name} foi matriculado na formação $nome.")
+        } else {
+            println("${usuario.name} já está matriculado na formação $nome.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Alice", "alice@example.com")
+    val usuario2 = Usuario("Bob", "bob@example.com")
+    val usuario3 = Usuario("Alisson", "Alisson@example.com")
+    
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 120)
+    val conteudo2 = ConteudoEducacional("Algoritmos Avançados", 180)
+    val conteudo3 = ConteudoEducacional("Paradigmas de programação", 120) 
+    val conteudo4 = ConteudoEducacional("Fundamentos administrativos",80)
+    
+    val formacao = Formacao("Ciência da Computação", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao2 = Formacao("Sistemas de Informação", listOf(conteudo1, conteudo2, conteudo4))
+    
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario1) // Trying to enroll the same user again
+    formacao2.matricular(usuario3)
+    
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos.joinToString(", ") { it.name }}")
+    println("Inscritos na formação ${formacao2.nome}: ${formacao2.inscritos.joinToString(", ") { it.name }}")
 }
